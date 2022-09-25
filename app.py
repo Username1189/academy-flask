f31bd65c5ae138ed2412c039af49832f7caeb3cb
Sanjay Muthu <muthu.solayappan@gmail.com>
2022-09-25 15:01:28 +0530

    Updated Server

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+@socketio.on('message')
+def handle_message(msg):
+	print('Received Message: ' + msg)
+	send(msg, broadcast=True)
 
 @app.route('/')
-def hello_world():
-    return "Hello Sammy!"
+def index():
+	return "Working"
+
+if __name__ == '__main__':
+	socketio.run(app, host="192.168.0.112")
